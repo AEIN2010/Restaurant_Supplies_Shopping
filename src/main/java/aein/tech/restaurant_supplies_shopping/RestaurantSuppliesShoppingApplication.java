4d3afa8cdbc5429204bd7bf96a8a64659ff3f903
Abdelrahman Nassar <97475507+AEIN2010@users.noreply.github.com>
2024-11-26 16:04:15 -0500

    Add files via upload
    adding main project files

---
+++ src/main/java/aein/tech/restaurant_supplies_shopping/RestaurantSuppliesShoppingApplication.java
@@ -0,0 +1,13 @@
+package aein.tech.restaurant_supplies_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSuppliesShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantSuppliesShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aein/tech/restaurant_supplies_shopping/RestaurantSuppliesShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package aein.tech.restaurant_supplies_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSuppliesShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
